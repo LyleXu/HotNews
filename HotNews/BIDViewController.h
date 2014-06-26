540ffd4d8c69e281c47bffb1e24774bd250164d9
Lyle <chanxl009@gmail.com>
2014-06-26 16:18:34 +0800

    hotnews project

---
+++ HotNews/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  HotNews
+//
+//  Created by gtcc on 6/26/14.
+//  Copyright (c) 2014 xc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
