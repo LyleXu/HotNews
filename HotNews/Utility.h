25ec5d8a3a37443d5d392e97dcf99343581504be
JasonOcean <wangshuntoday@gmail.com>
2015-04-27 15:46:53 +0800

    refine the add logic, the existed item can't be added again

---
+++ HotNews/Utility.h
@@ -0,0 +1,13 @@
+//
+//  Utility.h
+//  HotNews
+//
+//  Created by Jason on 15/4/27.
+//  Copyright (c) 2015年 xc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Utility : UIViewController
+
+@end
---
+++ HotNews/Utility.m
@@ -0,0 +1,13 @@
+//
+//  Utility.m
+//  HotNews
+//
+//  Created by Jason on 15/4/27.
+//  Copyright (c) 2015年 xc. All rights reserved.
+//
+
+#import "Utility.h"
+
+@implementation Utility
+
+@end
