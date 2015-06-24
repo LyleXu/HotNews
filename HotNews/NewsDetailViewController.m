a03194bbfa3cf1d7ddc16a5ecbc8fedb8cce3dee
JasonOcean <wangshuntoday@gmail.com>
2015-06-24 17:26:20 +0800

    v1 done

--- HotNews/NewsDetailViewController.m
+++ HotNews/NewsDetailViewController.m
@@ -29,9 +29,40 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    float maxWidth = 0.95 * [UIScreen mainScreen].applicationFrame.size.width;
     
-    //NSString * targetWebContent = [DataLayer GetRssContentByUrl:newsInfo.link];
-    NSString * targetWebContent = [DataLayer GetRssContentByTitle:newsInfo.title];
-    [self.webView loadHTMLString:targetWebContent baseURL:nil];
+    NSString* targetWebContent = [DataLayer GetRssContentByTitle:newsInfo.title];
+    NSString* cssString =
+    [NSString stringWithFormat:@"<style type='text/css'>img {width:%fpx; height: auto;}</style>",maxWidth];
+    //NSString* htmlString = [NSString stringWithFormat:@"%@%@", cssString, targetWebContent];
+    NSString* htmlString = [NSString stringWithFormat:@"<html><head></head><body>%@%@</body></html>", cssString, targetWebContent];
+    [self.webView loadHTMLString:htmlString baseURL:nil];
+    
+    
+//    [self.webView stringByEvaluatingJavaScriptFromString:
+//     @"var script = document.createElement('script');"
+//     "script.type = 'text/javascript';"
+//     "script.text = \"function ResizeImages() { "
+//     "alert('aaaaa');"
+//     "var myimg,oldwidth;"
+//     "var maxwidth = 20.0;"
+//     "for(i=0;i <document.images.length;i++){"
+//     "myimg = document.images[i];"
+//     "if(myimg.width > maxwidth){"
+//     "oldwidth = myimg.width;"
+//     "myimg.width = maxwidth;"
+//     "}"
+//     "}"
+//     "}\";"
+//     "document.getElementsByTagName('head')[0].appendChild(script);"];
+    
+//    [self.webView stringByEvaluatingJavaScriptFromString:
+//     @"var script = document.createElement('script');"
+//     "script.type = 'text/javascript';"
+//     "script.text = \"function ResizeImages() { "
+//     "alert('aaaaa');"
+//     "}\";"
+//     "document.getElementsByTagName('head')[0].appendChild(script);"];
+//    [self.webView stringByEvaluatingJavaScriptFromString:@"ResizeImages();"];
 }
 @end
